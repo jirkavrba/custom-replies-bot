fcc2585d779fbb7e34b5b7a06aecf76852976974
Jiří Vrba <jiri@vrba.dev>
2022-03-19 09:28:35 +0100

    Add guild and permissions check

--- src/main/kotlin/dev/vrba/customrepliesbot/discord/commands/CreateCustomReplyCommand.kt
+++ src/main/kotlin/dev/vrba/customrepliesbot/discord/commands/CreateCustomReplyCommand.kt
@@ -2,6 +2,7 @@ package dev.vrba.customrepliesbot.discord.commands
 
 import dev.vrba.customrepliesbot.services.CustomRepliesService
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
@@ -20,10 +21,18 @@ class CreateCustomReplyCommand(private val service: CustomRepliesService) : Slas
         val name = event.getOption("name")?.asString ?: throw IllegalArgumentException("Missing the name parameter")
         val trigger = event.getOption("trigger")?.asString ?: throw IllegalArgumentException("Missing the trigger parameter")
         val response = event.getOption("response")?.asString ?: throw IllegalArgumentException("Missing the response parameter")
-        val guild =  event.guild?.idLong ?: 0
+
+        if (!event.isFromGuild) {
+            return event.reply("Sorry, this command can be only used inside guilds").queue()
+        }
+
+        if (!event.member!!.hasPermission(Permission.MANAGE_SERVER)) {
+            return event.reply("Sorry, this command can be only used by administrators of this guild").queue()
+        }
 
         val interaction = event.deferReply().complete()
 
+        val guild =  event.guild?.idLong ?: 0
         val reply = service.createCustomReply(name, trigger, response, guild)
         val embed = EmbedBuilder()
             .setColor(0x57F287)
