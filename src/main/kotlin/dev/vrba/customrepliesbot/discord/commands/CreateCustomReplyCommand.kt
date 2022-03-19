896d2b7f1a3e6f338cd0e8eaa318ef038730480a
Jiří Vrba <jiri@vrba.dev>
2022-03-19 11:38:55 +0100

    Remove unused imports

--- src/main/kotlin/dev/vrba/customrepliesbot/discord/commands/CreateCustomReplyCommand.kt
+++ src/main/kotlin/dev/vrba/customrepliesbot/discord/commands/CreateCustomReplyCommand.kt
@@ -3,7 +3,6 @@ package dev.vrba.customrepliesbot.discord.commands
 import dev.vrba.customrepliesbot.entities.CustomReply
 import dev.vrba.customrepliesbot.repositories.CustomRepliesRepository
 import net.dv8tion.jda.api.EmbedBuilder
-import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
