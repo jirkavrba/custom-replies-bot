1e2138ea187f81af343a6e985379accb9d14d788
Jiří Vrba <jiri@vrba.dev>
2022-03-19 18:26:39 +0100

    Resolve issues with caching queries

--- src/main/kotlin/dev/vrba/customrepliesbot/CustomRepliesBotApplication.kt
+++ src/main/kotlin/dev/vrba/customrepliesbot/CustomRepliesBotApplication.kt
@@ -5,9 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
+@SpringBootApplication
 @EnableCaching
 @ConfigurationPropertiesScan(basePackages = ["dev.vrba.customrepliesbot.configuration"])
-@SpringBootApplication
 class CustomRepliesBotApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/dev/vrba/customrepliesbot/repositories/CustomRepliesRepository.kt
+++ src/main/kotlin/dev/vrba/customrepliesbot/repositories/CustomRepliesRepository.kt
@@ -1,7 +1,10 @@
 package dev.vrba.customrepliesbot.repositories
 
 import dev.vrba.customrepliesbot.entities.CustomReply
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.Caching
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
@@ -9,14 +12,35 @@ import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-@Cacheable("custom_replies")
 interface CustomRepliesRepository : PagingAndSortingRepository<CustomReply, UUID> {
 
+    @Cacheable("custom_replies")
     fun findAllByGuildId(guildId: Long): List<CustomReply>
 
+    @Cacheable("custom_replies_page")
     fun findAllByGuildId(guildId: Long, page: Pageable): Page<CustomReply>
 
+    @Cacheable("custom_replies_exists")
     fun existsByGuildIdAndName(guildId: Long, name: String): Boolean
 
+    @Cacheable("custom_reply")
     fun findByGuildIdAndName(guild: Long, name: String): CustomReply?
+
+    @Caching(
+        evict = [
+            CacheEvict("custom_replies", allEntries = true),
+            CacheEvict("custom_replies_page", allEntries = true),
+            CacheEvict("custom_replies_exists", allEntries = true),
+            CacheEvict("custom_reply", allEntries = true)
+        ]
+    )
+    override fun <S : CustomReply?> save(entity: S): S
+
+    @Caching(evict = [
+        CacheEvict("custom_replies", allEntries = true),
+        CacheEvict("custom_replies_page", allEntries = true),
+        CacheEvict("custom_replies_exists", allEntries = true),
+        CacheEvict("custom_reply", allEntries = true)
+    ])
+    override fun delete(reply: CustomReply)
 }
